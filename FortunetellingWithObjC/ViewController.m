b44c76ae6fadc618a16ee1933740945b4d9f2314
yuky_az <yuky.azuma@gmail.com>
2014-07-17 15:46:03 +0900

    FirstCommit

--- FortunetellingWithObjC/ViewController.m
+++ FortunetellingWithObjC/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    IBOutlet UILabel *animalLabel;
+    IBOutlet UILabel *fortuneLabel;
+    NSArray *animalArray;
+}
             
 
 @end
@@ -18,6 +23,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    animalArray = @[@"🐶", @"🐱", @"🐴", @"🐮", @"🐘", @"🐯"];
+}
+
+-(IBAction)buttonTapped:(id)sender
+{
+    NSUInteger length = [animalArray count];
+    int rand = arc4random() % length;
+    NSString *animal = animalArray[rand];
+    
+    animalLabel.text = animal;
+    
+    if ([animal isEqualToString:@"🐶"]) {
+        fortuneLabel.text = @"大吉だワン！";
+    }else if([animal isEqualToString:@"🐱"]){
+        fortuneLabel.text = @"中吉だニャー！";
+    }else if([animal isEqualToString:@"🐴"]){
+        fortuneLabel.text = @"吉だヒヒーン！";
+    }else if([animal isEqualToString:@"🐮"]){
+        fortuneLabel.text = @"末吉だモー！";
+    }else if([animal isEqualToString:@"🐘"]){
+        fortuneLabel.text = @"凶だゾウ！";
+    }else if([animal isEqualToString:@"🐯"]){
+        fortuneLabel.text = @"大凶だガオー！";
+    }
 }
 
 - (void)didReceiveMemoryWarning {
